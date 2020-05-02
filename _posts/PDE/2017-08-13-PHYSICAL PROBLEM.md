e7823252d9e473a7147aefb79923190c443d8220
liuxin21 <x399liu@edu.uwaterloo.ca>
2020-05-02 14:39:53 -0400

    new

--- _posts/PDE/2017-08-13-PHYSICAL PROBLEM.md
+++
@@ -1,9 +0,0 @@
----
-layout: post
-author: liuxin
-title: 3-物理问题的分类
-date: 2017-08-13
-category: PDE
-tags: partial differential equations 
----
-
--- _posts/cs/python/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-with open('test.txt', 'w') as f:
-    x = np.random.randint(51, size=1000)
-    for i in range(2):
-        np.savetxt(f,np.histogram(x, bins=30)[i].reshape(1,-1))
-        f.write("\n")
